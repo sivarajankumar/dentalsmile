2697195bc5dc845c03c2892ef9b3c3cc4b01fa53
nunu.nugraha13 <nunu.nugraha13@gmail.com>
2013-05-02 05:35:30 +0000

    NEW FORM DENTIST

---
+++ DentistForm.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace smileUp
+{
+    partial class DentistForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.txt_userid = new System.Windows.Forms.TextBox();
+            this.txt_fname = new System.Windows.Forms.TextBox();
+            this.txt_lname = new System.Windows.Forms.TextBox();
+            this.txt_birthplace = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txt_address1 = new System.Windows.Forms.TextBox();
+            this.txt_address2 = new System.Windows.Forms.TextBox();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.dt_birthdate = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "userid:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "fname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Iname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "birthdate:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "birthplace:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "gender:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "address 1:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 241);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "address 2:";
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(12, 332);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(80, 23);
+            this.btn_reset.TabIndex = 8;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(128, 332);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(84, 23);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // txt_userid
+            // 
+            this.txt_userid.Location = new System.Drawing.Point(94, 25);
+            this.txt_userid.Name = "txt_userid";
+            this.txt_userid.Size = new System.Drawing.Size(100, 20);
+            this.txt_userid.TabIndex = 10;
+            // 
+            // txt_fname
+            // 
+            this.txt_fname.Location = new System.Drawing.Point(94, 52);
+            this.txt_fname.Name = "txt_fname";
+            this.txt_fname.Size = new System.Drawing.Size(100, 20);
+            this.txt_fname.TabIndex = 11;
+            // 
+            // txt_lname
+            // 
+            this.txt_lname.Location = new System.Drawing.Point(94, 79);
+            this.txt_lname.Name = "txt_lname";
+            this.txt_lname.Size = new System.Drawing.Size(100, 20);
+            this.txt_lname.TabIndex = 12;
+            // 
+            // txt_birthplace
+            // 
+            this.txt_birthplace.Location = new System.Drawing.Point(94, 133);
+            this.txt_birthplace.Name = "txt_birthplace";
+            this.txt_birthplace.Size = new System.Drawing.Size(100, 20);
+            this.txt_birthplace.TabIndex = 14;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(103, 187);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txt_address1
+            // 
+            this.txt_address1.Location = new System.Drawing.Point(94, 215);
+            this.txt_address1.Name = "txt_address1";
+            this.txt_address1.Size = new System.Drawing.Size(100, 20);
+            this.txt_address1.TabIndex = 17;
+            // 
+            // txt_address2
+            // 
+            this.txt_address2.Location = new System.Drawing.Point(94, 241);
+            this.txt_address2.Name = "txt_address2";
+            this.txt_address2.Size = new System.Drawing.Size(100, 20);
+            this.txt_address2.TabIndex = 18;
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(94, 293);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(100, 20);
+            this.txt_phone.TabIndex = 47;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 293);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 46;
+            this.label10.Text = "phone";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 13);
+            this.label9.TabIndex = 45;
+            this.label9.Text = "city";
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(94, 267);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(100, 20);
+            this.txt_city.TabIndex = 44;
+            // 
+            // dt_birthdate
+            // 
+            this.dt_birthdate.Location = new System.Drawing.Point(94, 106);
+            this.dt_birthdate.Name = "dt_birthdate";
+            this.dt_birthdate.Size = new System.Drawing.Size(100, 20);
+            this.dt_birthdate.TabIndex = 48;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(103, 163);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(96, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 58);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 314);
+            this.groupBox2.TabIndex = 49;
+            this.groupBox2.TabStop = false;
+            // 
+            // DentistForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 376);
+            this.Controls.Add(this.dt_birthdate);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_city);
+            this.Controls.Add(this.txt_address2);
+            this.Controls.Add(this.txt_address1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txt_birthplace);
+            this.Controls.Add(this.txt_lname);
+            this.Controls.Add(this.txt_fname);
+            this.Controls.Add(this.txt_userid);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "DentistForm";
+            this.Text = "DoctorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.TextBox txt_userid;
+        private System.Windows.Forms.TextBox txt_fname;
+        private System.Windows.Forms.TextBox txt_lname;
+        private System.Windows.Forms.TextBox txt_birthplace;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox txt_address1;
+        private System.Windows.Forms.TextBox txt_address2;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.DateTimePicker dt_birthdate;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ DentistForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace smileUp
+{
+    public partial class DentistForm : Form
+    {
+        private DentalSmileDB dbConnect;
+
+        public DentistForm()
+        {
+            InitializeComponent();
+            dbConnect = new DentalSmileDB();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+
+            string userid = txt_userid.Text;
+            string fname = txt_fname.Text;
+            string lname = txt_lname.Text;
+            DateTime birthdate = DateTime.Parse(dt_birthdate.Text);
+            string birthplace = txt_birthplace.Text;
+            string gender;
+            string address1 = txt_address1.Text;
+            string address2 = txt_address2.Text;
+            string city = txt_city.Text;
+            string phone = txt_phone.Text;
+            string created;
+            string createdBy;
+
+            dbConnect.InsertDentist(userid, fname, lname, birthdate, birthplace, address1, address2, city, phone);
+
+            //MessageBox.Show("Data Patient is saved!");
+            DialogResult result = MessageBox.Show("Data Dentist is saved successfully", "Continue to Scanning?",
+            MessageBoxButtons.OKCancel);
+            switch (result)
+            {
+                case DialogResult.OK:
+                    {
+                        //this.Text = "[OK]";
+                        this.Close();
+                        break;
+                    }
+                case DialogResult.Cancel:
+                    {
+                        this.Text = "[Cancel]";
+                        break;
+                    }
+            }
+        }
+
+    }
+}
