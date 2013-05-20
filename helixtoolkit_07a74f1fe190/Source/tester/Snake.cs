9a5347769352badfd66566294e5617d2dcc6e5eb
edui.bin <edui.bin@gmail.com>
2013-05-20 07:54:52 +0000

    integration - scanner,modelviewer,helix

---
+++ helixtoolkit_07a74f1fe190/Source/tester/Snake.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace tester
+{
+    class Snake
+    {
+        /*
+        public double Energy()
+        {
+            List<int> elist = new List<int>();
+            int n = 1; //s.count
+            for(var i=0; i< n; i++){
+                //minimalvalue of e_si
+                //e_si = eInt_si + eExt_si
+            }
+            return 0;
+        }
+        public double InternalEnergy(double a, double Xs, double b, double Xss)
+        {
+            double Tension = a * Math.Pow(Math.Abs(Xs), 2);
+            double Stiffness = b * Math.Pow(Math.Abs(Xss), 2);
+
+            return Tension + Stiffness;
+        }
+
+        public double EksternalEnergy()
+        {
+            double eFeat_si = 0;
+            double ePress_si = 0;
+            
+            double eExt_si = eFeat_si + ePress_si;
+            
+            return eExt_si;
+        }
+
+        public double meanCurvature(Point3D p)
+        {
+            double k1 = 0;
+            double k2 = 0;
+
+            double kH = (k1 + k2) / 2;
+            
+            return kH;
+        }
+        public double unitTangentVector(double s1, double s11)
+        {
+            double t = s1 - s11 / Math.Sqrt(s1 - s11);
+            return t;
+        }
+        public double outerProduct()
+        {
+            double b = unitTangentVector * normal;
+            return b;
+        }
+        public double edgeTangetFlow()
+        {
+            //N_v = neighbour vertices of vertices v
+            double sumN_v = 0;
+            for(var u=0;u < N_v.count; u++){
+                sumN_v += (k_v - k_u) * ((u - v) / Math.Sqrt(u -v) ;
+            }
+
+            double d_v = 1/N_v.count * sumN_v;
+            double n_v = 0;
+            
+            double tETF_v = d_v * n_v;
+            
+            return tETF_v;
+        }
+         */ 
+
+    }
+}
